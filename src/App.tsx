5ab59f3b06ca0fffba49268ca5995787f65dedab
Abduhamid <Abdukhamid000@gmail.com>
2026-03-23 07:32:13 +0500

    remove div

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div className="flex flex-col items-center mt-5">
-        <div className="bg-blue-500 rounded-full size-4 mt-16"></div>
-      </div>
+      <div className="flex flex-col items-center mt-5"></div>
     </div>
   );
 }
