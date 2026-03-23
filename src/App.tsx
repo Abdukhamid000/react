ea83cd0e62ebd73513febf097507abfc75771434
Abduhamid <Abdukhamid000@gmail.com>
2026-03-23 06:53:32 +0500

    changes after let's see

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div>
       <div className="flex flex-col items-center mt-5">
-        <div className="h-64 bg-gray-300 w-1"></div>
         <div className="bg-blue-500 rounded-full size-4 -mt-2"></div>
         <div className="bg-blue-500 rounded-full size-4 mt-16"></div>
       </div>
