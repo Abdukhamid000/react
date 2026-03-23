d1ef83c6253bc4ddef4863c23587d753f2c5dd17
Abduhamid <Abdukhamid000@gmail.com>
2026-03-23 07:10:27 +0500

    last

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div>
       <div className="flex flex-col items-center mt-5">
-        <div className="bg-blue-500 rounded-full size-4 -mt-2"></div>
         <div className="bg-blue-500 rounded-full size-4 mt-16"></div>
       </div>
     </div>
