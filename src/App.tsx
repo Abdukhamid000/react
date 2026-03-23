cce18d83f9cce72cb17d1757cf82fca61038c77f
Abduhamid <Abdukhamid000@gmail.com>
2026-03-23 06:52:44 +0500

    let's see

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./App.css";
+
 function App() {
-  return <div>NEW APP {import.meta.env.VITE_BASE_URL}</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-5">
+        <div className="h-64 bg-gray-300 w-1"></div>
+        <div className="bg-blue-500 rounded-full size-4 -mt-2"></div>
+        <div className="bg-blue-500 rounded-full size-4 mt-16"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
